f582eca06d1982d18a0d9a0edaaefcce9faaa080
Roma <andrushchenkoroman@gmail.com>
2022-12-14 13:44:45 +0200

    deleted files

--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Content from './Content'
-
-type TitleProps = {
-    title: string
-    text?: string
-}
-
-const Title = ({ title, text = 'TEST' }: TitleProps) => {
-    return (
-        <h1>
-            {text} {title}
-        </h1>
-    )
-}
 
 const App = () => {
-    return (
-        <>
-            <Title text="Hello" title="React" />
-            <Content
-                p1="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
-            officia facere obcaecati sed omnis ad, unde corrupti laboriosam?
-            Voluptas rerum consequatur neque iste distinctio voluptate
-            dolorem cum rem corrupti repellat."
-                p2="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
-            officia facere obcaecati sed omnis ad, unde corrupti laboriosam?
-            Voluptas rerum consequatur neque iste distinctio voluptate
-            dolorem cum rem corrupti repellat."
-            />
-        </>
-    )
+    return <></>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
