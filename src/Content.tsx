b3139b1a1f8367f57c40843c634735d5736f9003
Roma <andrushchenkoroman@gmail.com>
2022-12-14 13:11:47 +0200

    add css

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,3 +1,4 @@
+import './Content.css'
 type Props = {
     p1: string
     p2: string
@@ -5,7 +6,9 @@ type Props = {
 const Content = ({ p1, p2 }: Props) => {
     return (
         <>
-            <p>{p1}</p>
+            <p className="tomato">
+                <p>{p1}</p>
+            </p>
             <p>{p2}</p>
         </>
     )
