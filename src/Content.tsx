e66a4c201e9af5b0e7b796e8247441caf7fbe646
Roma <andrushchenkoroman@gmail.com>
2022-12-14 12:30:54 +0200

    add import

---
+++ src/Content.tsx
@@ -0,0 +1,20 @@
+const Content = () => {
+    return (
+        <>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptatem aliquid repudiandae, quo iure, necessitatibus
+                consequuntur reiciendis ipsa exercitationem mollitia voluptate
+                doloribus nam? Laudantium nulla iste neque, et dolores expedita
+                nisi.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
+                vitae eum magnam ipsam nihil repellat sequi, corporis odit
+                consequuntur neque hic est error commodi eligendi ea
+                perspiciatis amet officiis harum.
+            </p>
+        </>
+    )
+}
+export default Content
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Content from './Content'
 
 type TitleProps = {
     title: string
@@ -14,33 +15,10 @@ const Title = ({ title, text = 'TEST' }: TitleProps) => {
     )
 }
 
-const Content = () => {
-    return (
-        <>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptatem aliquid repudiandae, quo iure, necessitatibus
-                consequuntur reiciendis ipsa exercitationem mollitia voluptate
-                doloribus nam? Laudantium nulla iste neque, et dolores expedita
-                nisi.
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
-                vitae eum magnam ipsam nihil repellat sequi, corporis odit
-                consequuntur neque hic est error commodi eligendi ea
-                perspiciatis amet officiis harum.
-            </p>
-        </>
-    )
-}
-
 const App = () => {
     return (
         <>
             <Title text="Hello" title="React" />
-            <Title text="Hi" title="About" />
-            <Title title="Bla-bla" />
-
             <Content />
         </>
     )
