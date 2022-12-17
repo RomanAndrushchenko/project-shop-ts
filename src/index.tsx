5daafd4f4ab7fef369ec4720048d25844cac7ae1
Roma <andrushchenkoroman@gmail.com>
2022-12-18 00:13:41 +0200

    add mui

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Content from './header'
-
-const App = () => {
-    return (
-        <>
-            <Content />
-        </>
-    )
-}
+import App from './container/App/App'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
