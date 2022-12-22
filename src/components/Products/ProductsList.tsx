74bd7e35040b1d2e8b5a27fd89a1031e7b2592b1
Roma <andrushchenkoroman@gmail.com>
2022-12-22 18:24:34 +0200

    props dinamic

--- src/components/Products/ProductsList.tsx
+++ src/components/Products/ProductsList.tsx
@@ -6,16 +6,16 @@ const ProductsList = (props: Props) => {
         <>
             <Grid container direction="row" alignItems="center" spacing={4}>
                 <Grid item xs={12} sm={6} md={4}>
-                    <ProductsListItem />
+                    <ProductsListItem title="iPhone X" ram="256 GB" />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
-                    <ProductsListItem />
+                    <ProductsListItem title="iPhone XS" ram="128 GB" />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
-                    <ProductsListItem />
+                    <ProductsListItem title="iPhone 12" ram="64 GB" />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
-                    <ProductsListItem />
+                    <ProductsListItem title="iPhone 14 PRO" ram="512 GB" />
                 </Grid>
             </Grid>
         </>
--- src/components/Products/ProductsListItem.tsx
+++ src/components/Products/ProductsListItem.tsx
@@ -1,17 +1,17 @@
 import { Card, CardActions, CardContent, Button } from '@mui/material'
 import './ProductListItem.scss'
-type Props = {}
+type Props = { title: string; ram: string }
 const ProductsListItem = (props: Props) => {
     return (
         <Card className="product-list-item">
             <CardContent>
-                <h4 className="product-title">iPhone X</h4>
-                <p className="product-desc">This is iphone X</p>
+                <h4 className="product-title">{props.title}</h4>
+                <p className="product-desc">This is {props.title}</p>
                 <div className="product-features">
                     <span>Type:</span> phone
                 </div>
                 <div className="product-features">
-                    <span>Capacity:</span> 64 gb
+                    <span>Capacity:</span> {props.ram}
                 </div>
                 <div className="product-price">500$</div>
             </CardContent>
