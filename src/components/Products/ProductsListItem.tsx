d497fe672b89330213a4a6a5300ed143e2d0e891
Roma <andrushchenkoroman@gmail.com>
2022-12-25 09:46:12 +0200

    add state count

--- src/components/Products/ProductsListItem.tsx
+++ src/components/Products/ProductsListItem.tsx
@@ -5,6 +5,7 @@ import {
     Button,
     TextField,
 } from '@mui/material'
+import { useState } from 'react'
 import './ProductListItem.scss'
 
 type Props = {
@@ -23,6 +24,14 @@ const ProductsListItem = ({
     price,
     image,
 }: Props) => {
+    const [count, setCount] = useState<number>(1)
+
+    const onIncrement = () => {
+        setCount((prevState: number) => prevState + 1)
+    }
+    const onDecrement = () => {
+        setCount((prevState: number) => prevState - 1)
+    }
     return (
         <Card className="product-list-item">
             <CardContent>
@@ -39,9 +48,13 @@ const ProductsListItem = ({
                 </div>
                 <div className="product-price">{price}</div>
                 <div className="product-quantity">
-                    <Button variant="outlined">-</Button>
-                    <TextField size="small" value="1" />
-                    <Button variant="outlined">+</Button>
+                    <Button variant="outlined" onClick={() => onDecrement()}>
+                        -
+                    </Button>
+                    <TextField size="small" value={count} />
+                    <Button variant="outlined" onClick={() => onIncrement()}>
+                        +
+                    </Button>
                 </div>
             </CardContent>
             <CardActions className="btns-wrap">
